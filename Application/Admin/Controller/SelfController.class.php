27b8529e02e974885b75dc081f1a9e3f86447bda
yang hu <wo646231194@live.com>
2016-01-13 11:18:33 +0800

    2016-01-13 Admin Self 完成 图片裁剪

--- Application/Admin/Controller/SelfController.class.php
+++ Application/Admin/Controller/SelfController.class.php
@@ -5,10 +5,14 @@ class SelfController extends BaseController {
 	public function index() {
 		$this -> display();
 	}
-
+	/**
+	 * @param file,fname,id
+	 * 图片上传
+	 * */
 	public function upload() {
 		$fname = I('post.fname');
-		if(empty($fname)){
+		$id = I('post.user_id');
+		if(empty($fname)||empty($id)){
 			$this->error("数据为空");
 		}
 		$upload = new \Think\Upload();
@@ -22,14 +26,46 @@ class SelfController extends BaseController {
 		$upload -> savePath = '';
 		// 设置附件上传（子）目录
 		$upload -> subName = $fname;
-		//设置子目录名
+		// 设置子目录名
+		$upload -> saveName = $fname;
 		// 上传文件
+		$upload -> replace = TRUE;
+		// 允许覆盖上传
 		$info = $upload -> upload();
-		if ($info) {// 上传成功
+		//trace($info);
+		$adminUser = M('AdminUser');
+		$data = $adminUser -> where('id = %d',$id)->find();
+		$data['img'] = '/Images/User/'.$fname.'/'.$info['photo']['savename'];
+		$status = $adminUser -> save($data);
+		if ($info && $status) {// 上传成功
 			$this->redirect('Admin/Self/index');
 		} else {// 上传错误提示错误信息
-			$this->error($upload->getError());
+			$this->redirect('Admin/Self/index');
 		}
 	}
-
+	/**
+	 * 
+	 * 头像保存
+	 * */
+	public function save(){
+		$imgX = I('post.imgX');
+		$imgY = I('post.imgY');
+		$imgW = I('post.imgW');
+		$imgH = I('post.imgH');
+		$path = I('post.path');
+		if($imgX==''||$imgY==''||$imgW==''||$imgH==''||$path==''){
+			$this -> ajaxReturn("数据为空");
+		}
+		$image = new \Think\Image();
+		$file = './Public'.$path;
+		//trace($file);
+		$image -> open($file);
+		$status = $image->crop($imgW, $imgH,$imgX,$imgY)->save($file);
+		if($status){
+			$this -> ajaxReturn(TRUE);
+		}else{
+			$msg = "保存失败";
+			$this -> ajaxReturn($msg);
+		}
+	}
 }
