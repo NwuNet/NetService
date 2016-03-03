542753a176c7cd8a93588b59b844f8b656966a09
yang hu <wo646231194@live.com>
2016-03-03 21:08:51 +0800

    2016-03-03 Boss SetAsset 添加工具选项设置 名称 品牌 型号

--- Application/Boss/Controller/SetAssetController.class.php
+++ Application/Boss/Controller/SetAssetController.class.php
@@ -4,7 +4,39 @@ use Think\Controller;
 class SetAssetController extends BaseController {
     // --------------------工具---------------------
     public function tool(){
+        $select = M('ToolSelect');
+        $tool = $select->select();//获取数量
+        $toolselect = $select->order('name')->limit(count($tool))->select();
+        $this->assign('toolselect',$toolselect);
         $this->display();
+    }
+    // --------------------工具select添加和修改---------------------
+    public function tooladd(){
+        $name = I('post.name');//名称
+        $brand = I('post.brand');//品牌
+        $model = I('post.model');//型号
+        if($name==''||$brand==''||$model==''){
+            $this->ajaxReturn("数据为空");
+        }
+        $id = I('post.id');
+        $select = M('ToolSelect');
+        $select->create();
+        if($id==''){ //添加
+            if($select->add()){
+                $this->ajaxReturn(true);
+            }else{
+                $msg = '添加失败';
+                $this->ajaxReturn($msg);
+            }
+        }else{  //修改
+            if($select->save()){
+                $this->ajaxReturn(true);
+            }else{
+                $msg = '修改失败';
+                $this->ajaxReturn($msg);
+            }
+        }
+
     }
 	// --------------------耗材---------------------
 	public function exhaust(){
