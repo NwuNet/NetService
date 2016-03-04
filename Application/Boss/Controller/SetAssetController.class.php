28eb570c1e0c31fc5fb8348ab4187ea057866aa4
yang hu <wo646231194@live.com>
2016-03-04 11:16:19 +0800

    2016-03-04 Boss SetAsset 添加工具状态设置 名称 描述 status

--- Application/Boss/Controller/SetAssetController.class.php
+++ Application/Boss/Controller/SetAssetController.class.php
@@ -8,6 +8,9 @@ class SetAssetController extends BaseController {
         $tool = $select->select();//获取数量
         $toolselect = $select->order('name')->limit(count($tool))->select();
         $this->assign('toolselect',$toolselect);
+        $state = M('ToolState');
+        $toolstate = $state->select();
+        $this->assign('toolstate',$toolstate);
         $this->display();
     }
     // --------------------工具select添加和修改---------------------
@@ -36,7 +39,46 @@ class SetAssetController extends BaseController {
                 $this->ajaxReturn($msg);
             }
         }
-
+    }
+    // --------------------工具状态添加和修改---------------------
+    public function toolstate(){
+        if(I('post.name')==''||I('post.label')==''||I('post.icon')==''||I('post.status')==''){
+            $this->ajaxReturn("数据为空");
+        }
+        $id = I('post.id');
+        $state = M('ToolState');
+        $state->create();
+        if($id==''){ //添加
+            if($state->add()){
+                $this->ajaxReturn(true);
+            }else{
+                $msg = '添加失败';
+                $this->ajaxReturn($msg);
+            }
+        }else{  //修改
+            if($state->save()){
+                $this->ajaxReturn(true);
+            }else{
+                $msg = '修改失败';
+                $this->ajaxReturn($msg);
+            }
+        }
+    }
+    // --------------------工具状态添加和修改---------------------
+    public function toolstateflip(){
+        if(I('post.id')==''){
+            $this->ajaxReturn("数据为空");
+        }
+        $id = I('post.id');
+        $state = M('ToolState');
+        $data = $state->where('id=%d',$id)->find();
+        $data['status'] = $data['status']==1?0:1;
+        if($state->save($data)){
+            $this->ajaxReturn(true);
+        }else{
+            $msg = '修改失败';
+            $this->ajaxReturn($msg);
+        }
     }
 	// --------------------耗材---------------------
 	public function exhaust(){
@@ -58,6 +100,7 @@ class SetAssetController extends BaseController {
 	public function overall(){
         $this->display();
     }
+
 	// --------------------空操作---------------------
 	public function _empty($name){
 		echo "Not Found!";
