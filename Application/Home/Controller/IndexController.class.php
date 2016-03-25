6edafc1c9739143ed1ab082642e231ccdd75ea9f
kangxinian <934050201@qq.com>
2016-03-25 10:34:13 +0800

    2016-03-25 Home 修改base_bt.html 未能实现预约维修添加

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,6 +5,31 @@ class IndexController extends Controller {
     public function index(){
         $this->display();
     }
+	// --------------------添加服务单---------------------
+	public function servicecardadd(){
+		$name = I('post.name');
+		$student_no = I('post.student_no');
+		$phone = I('post.phone');
+		$building = I('post.building');
+		$room = I('post.room');
+		$question = I('post.question');
+		$description = I('post.description');
+		$appointment_time = I('post.appointment_time');
+
+		if($name==''||$student_no==''||$phone==''||$building==''||$room==''||$question==''||$description==''||$appointment_time==''){
+			$this->ajaxReturn("数据为空");
+		}
+		$card = M('ServiceCard');		
+		$card->dormitory = $building.'-'.$room ;
+		$card->start  = date("Y-m-d H:i:s",NOW_TIME);
+		$card->create();
+		$card->add();
+		if($card){
+			$this->ajaxReturn(true);
+		}else{
+			$this->ajaxReturn("添加失败");
+		}
+	}
 	public function download(){
         $this->display();
     }
