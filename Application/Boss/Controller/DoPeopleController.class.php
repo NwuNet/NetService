aab3252283c8ab620e0a8a373b57fd6ef5c8e2b4
kangxinian <934050201@qq.com>
2016-04-27 13:49:26 +0800

    2016-04-27 Boss DoPeople 添加离职审批

--- Application/Boss/Controller/DoPeopleController.class.php
+++ Application/Boss/Controller/DoPeopleController.class.php
@@ -108,7 +108,7 @@ class DoPeopleController extends BaseController {
     public function query(){
         $this->display();
     }
-	// --------------------员工管理---------------------
+	// --------------------员工请假管理---------------------
     public function staff(){
     	
 		$Staff = M('StaffVacation');
@@ -119,7 +119,7 @@ class DoPeopleController extends BaseController {
 	
 	// --------------------员工请假条审批---------------------
 	public function vacationstatus(){
-		$vacation_id = I('post.id');
+		$vacation_id = I('post.vacation_id');
 		$operator = I('post.operator');
 		$approve = I('post.approve');
 				
@@ -167,8 +167,8 @@ class DoPeopleController extends BaseController {
 				case 3 :$orderSql = " end_time " . $order_dir;break;
 				case 4 :$orderSql = " reason " . $order_dir;break;
 				case 5 :$orderSql = " time " . $order_dir;break;
-			/*	case 6 :$orderSql = " approve " . $order_dir;break;
-	            case 7 :$orderSql = " approve_time " . $order_dir;break;*/
+				case 6 :$orderSql = " approve " . $order_dir;break;
+	            case 7 :$orderSql = " approve_time " . $order_dir;break;
 	            default :$orderSql = '';
 	        }
 	    }
@@ -181,7 +181,7 @@ class DoPeopleController extends BaseController {
 	    //表的总记录数 必要
 	    $recordsTotal = $vacationState->count();
 	
-	    $map['id|uname|start_time|end_time|reason|time']=array('like',"%".$search."%");//|approve|approve_time
+	    $map['id|uname|start_time|end_time|reason|time|approve|approve_time']=array('like',"%".$search."%");
 	    if(strlen($search)>0){
 	        $recordsFiltered = count($vacationState->where($map)->select());
 	        $table = $vacationState->where($map)->order($orderSql)->limit($start.','.$length)->select();
@@ -192,7 +192,101 @@ class DoPeopleController extends BaseController {
 	
 	    $infos = array();
 	    foreach($table as $row){
-	        $obj = array($row['id'],$row['uname'],$row['start_time'],$row['end_time'],$row['reason'],$row['time']);//,$row['approve'],$row['approve_time']
+	        $obj = array($row['id'],$row['uname'],$row['start_time'],$row['end_time'],$row['reason'],$row['time'],$row['approve'],$row['approve_time']);
+	        array_push($infos,$obj);
+	    }
+	
+	    $this->ajaxReturn(array(
+	        "draw" => intval($draw),
+	        "recordsTotal" => intval($recordsTotal),
+	        "recordsFiltered" => intval($recordsFiltered),
+	        "data" => $infos
+	    ));
+	}
+    // --------------------员工离职管理---------------------
+    public function dimission(){
+    	
+		$DimissionStaff = M('Dimission');
+		$Dimission = $DimissionStaff->where('status=0')->select();
+		$this->assign('dimission',$Dimission);//
+        $this->display();
+    }
+	
+	// --------------------员工请假条审批---------------------
+	public function dimissionstatus(){
+		$dimission_id = I('post.dimission_id');
+		$operator = I('post.operator');
+		$approve = I('post.approve');
+		$dimission_time = I('post.dimission_time');
+				
+		if($dimission_id==''||$approve==''){
+			$this->ajaxReturn("数据为空");
+		}								
+		$State = M('DimissionState');
+		$State->create();	
+		$State->time  = date("Y-m-d H:i:s",NOW_TIME);
+		$State->add();
+		if($State){
+			$Dimission = M('Dimission');
+   		    $Dimission->id=$dimission_id;
+		    $Dimission->status=1;
+			$Dimission->save();
+			if($Dimission){
+				$this->ajaxReturn(true);
+		    }else{
+			$this->ajaxReturn("审批失败");
+		    }			
+		}else{
+			$this->ajaxReturn("审批失败");
+		}
+	}	
+	public function dimissiontatetable() {
+	    $dimissionState = D('DimissionView');
+	
+	    //获取Datatables发送的参数 必要
+	    $draw = I('get.draw');//这个值作者会直接返回给前台
+	
+	    //排序
+	    $order_column = I('get.order')['0']['column'];//那一列排序，从0开始
+	    $order_dir = I('get.order')['0']['dir'];//ase desc 升序或者降序
+	    //拼接排序sql
+	    $orderSql = "";
+	    if (isset($order_column)) {
+	        $i = intval($order_column);
+	        switch($i) {
+	            case 0 :$orderSql = " id " . $order_dir;break;
+	            case 1 :$orderSql = " uname " . $order_dir;break;
+				case 2 :$orderSql = " position " . $order_dir;break;
+	            case 3 :$orderSql = " start_time " . $order_dir;break;
+				case 4 :$orderSql = " end_time " . $order_dir;break;
+				case 5 :$orderSql = " reason " . $order_dir;break;
+				case 6 :$orderSql = " approve_time " . $order_dir;break;
+				case 7 :$orderSql = " approve " . $order_dir;break;
+	            case 8 :$orderSql = " time " . $order_dir;break;
+	            default :$orderSql = '';
+	        }
+	    }
+	
+	    //搜索
+	    $search = $_GET['search']['value'];//获取前台传过来的过滤条件
+	    //分页
+	    $start = $_GET['start'];//从多少开始
+	    $length = $_GET['length'];//数据长度
+	    //表的总记录数 必要
+	    $recordsTotal = $dimissionState->count();
+	
+	    $map['id|uname|position|start_time|end_time|reason|approve_time|approve|time']=array('like',"%".$search."%");
+	    if(strlen($search)>0){
+	        $recordsFiltered = count($dimissionState->where($map)->select());
+	        $table = $dimissionState->where($map)->order($orderSql)->limit($start.','.$length)->select();
+	    }else{
+	        $recordsFiltered = $recordsTotal;
+	        $table = $dimissionState->order($orderSql)->limit($start.','.$length)->select();
+	    }
+	
+	    $infos = array();
+	    foreach($table as $row){
+	        $obj = array($row['id'],$row['uname'],$row['position'],$row['start_time'],$row['end_time'],$row['reason'],$row['approve_time'],$row['approve'],$row['time']);
 	        array_push($infos,$obj);
 	    }
 	
--- Application/Boss/Model/StaffVacationViewModel.php
+++ Application/Boss/Model/StaffVacationViewModel.php
@@ -7,12 +7,22 @@ use Think\Model\ViewModel;
 
 class StaffVacationViewModel extends ViewModel {
 
-	public $viewFields = array(
+	/*public $viewFields = array(
 	'StaffVacation' => array('id','uname','start_time','end_time','reason','time'),
 	'VacationState' => array(			
 		'approve' => 'approve',
 		'operator' => 'operator',
 	//	'approve_time' => 'time', 
+		'_on' => 'VacationState.vacation_id=StaffVacation.id'), 
+	    
+	);*/
+	public $viewFields = array(
+	'VacationState' => array('id','approve','time'),
+	'StaffVacation' => array(
+		'uname'=> 'uname',
+       'start_time'=> 'start_time',
+       'end_time'=> 'end_time',
+        'reason'=> 'reason',				 
 		'_on' => 'VacationState.vacation_id=StaffVacation.id'), 
 	    
 	);
