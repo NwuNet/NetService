ba2fe839ad467b325cebf1e58c6f26b138309245
yang hu <wo646231194@live.com>
2016-05-07 14:36:51 +0800

    2016-05-07 Staff Index 修改待维修服务单呈现方式

--- Application/Boss/Controller/IndexController.class.php
+++ Application/Boss/Controller/IndexController.class.php
@@ -5,7 +5,7 @@ class IndexController extends BaseController{
 
     public function index(){
         $Card = M('ServiceCard');
-        $servicecardinfo = $Card->where('status = 0')->select();
+        $servicecardinfo = $Card->where('status = 0')->order('dormitory')->select();
         $this -> assign('servicecardinfo',$servicecardinfo );//服务单表
 
         $staff =D('Admin/StaffUserView')->where('status = 1')->field('uname')->select();
--- Application/Staff/Controller/IndexController.class.php
+++ Application/Staff/Controller/IndexController.class.php
@@ -3,6 +3,13 @@ namespace Staff\Controller;
 use Think\Controller;
 class IndexController extends BaseController {
     public function index(){
+        $Card = M('ServiceCard');
+        $servicecardinfo = $Card->where('status = 0')->order('dormitory')->select();
+        $this -> assign('servicecardinfo',$servicecardinfo );//服务单表
+
+        $staff =D('Admin/StaffUserView')->where('status = 1')->field('uname')->select();
+        $this->assign('staff',$staff);
+
         $this->display();
     }
 }
