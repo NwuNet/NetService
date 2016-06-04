73fac1c999fe7f557f6309ea3060d8ae7211c738
wo646231194 <wo646231194@live.com>
2016-06-04 18:16:33 +0800

    2016-06-04 Boss Widget schedule  排版表插件添加校区属性

--- Application/Boss/Widget/StaticWidget.class.php
+++ Application/Boss/Widget/StaticWidget.class.php
@@ -3,8 +3,9 @@ namespace Boss\Widget;
 use Think\Controller;
 class StaticWidget extends Controller {
     public function schedule(){
+        $loginService = D('Login','Service')->getuserInfo();//user
         $schStaff = M('ScheduleStaff');
-        $removestaff = $schStaff->where('status =1')->select();
+        $removestaff = $schStaff->where('status =1 and area = "%s"',$loginService['area'])->select();
         $this->assign('removestaff',$removestaff);
         $this->display('Static:schedule');
     }
