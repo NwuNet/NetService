22150829f94db4090ee15e7032fb78202e524493
wo646231194 <wo646231194@live.com>
2016-06-06 14:37:57 +0800

    2016-06-06 Staff-DoService-servicequery bug 添加 校区属性

--- Application/Staff/Controller/DoServiceController.class.php
+++ Application/Staff/Controller/DoServiceController.class.php
@@ -25,7 +25,7 @@ class DoServiceController extends BaseController {
 			switch($i) {
 				case 0 :$orderSql = " id " . $order_dir;break;
 				case 1 :$orderSql = " name " . $order_dir;break;
-				case 2 :$orderSql = " student_no " . $order_dir;break;
+				case 2 :$orderSql = " area " . $order_dir;break;
 				case 3 :$orderSql = " dormitory " . $order_dir;break;
 				case 4 :$orderSql = " phone " . $order_dir;break;
 				case 5 :$orderSql = " description " . $order_dir;break;
@@ -44,7 +44,7 @@ class DoServiceController extends BaseController {
 		//表的总记录数 必要
 		$recordsTotal = $Card->count();
 
-		$map['id|name|student_no|dormitory|phone|description|start|appointment_time|status']=array('like',"%".$search."%");		
+		$map['id|name|area|dormitory|phone|description|start|appointment_time|status']=array('like',"%".$search."%");
 		if(strlen($search)>0){
 			$recordsFiltered = count($Card->where($map)->select());
 			$table = $Card->where($map)->order($orderSql)->limit($start.','.$length)->select();
@@ -55,7 +55,7 @@ class DoServiceController extends BaseController {
 
 		$infos = array();
 		foreach($table as $row){
-			$obj = array($row['id'],$row['name'],$row['student_no'],$row['dormitory'],$row['phone'],$row['description'],$row['start'],$row['appointment_time'],$row['status']);
+			$obj = array($row['id'],$row['name'],$row['area'],$row['dormitory'],$row['phone'],$row['description'],$row['start'],$row['appointment_time'],$row['status']);
 			array_push($infos,$obj);
 		}
 
