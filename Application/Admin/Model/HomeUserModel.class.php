b3714455d3c199cb02b628efd8a79669b3b7fe83
wo646231194 <wo646231194@live.com>
2016-06-08 14:28:15 +0800

    2016-06-07 Home-User-Model bug 修复密码多次计算md5

--- Application/Admin/Model/HomeUserModel.class.php
+++ Application/Admin/Model/HomeUserModel.class.php
@@ -20,7 +20,11 @@ class HomeUserModel extends Model{
 	 public function plus($data){
 	 	if($this->where('id = %d ',$data['id'])->find()) return FALSE;
 //		 $this -> uname = $data['uname'];
-		 $data['password'] = md5($data['password']);
+		 if(strlen(md5($data['password'])) == strlen($data['password'])){
+			 $this -> password = $data['password'];
+		 }else{
+			 $this -> password = md5($data['password']);
+		 }
 		 $data['begintime'] = date("Y-m-d H:i:s",NOW_TIME);
 		 $data['status'] = 1;
 //	 	$this -> user_id = $data['user_id'];
@@ -37,9 +41,13 @@ class HomeUserModel extends Model{
 	  * */
 	 public function edit($data){
 	     if(count($this->where('id = %d ',$data['id'] )->find())>0) {
-
+			 if(strlen(md5($data['password'])) == strlen($data['password'])){
+				 $this -> password = $data['password'];
+			 }else{
+				 $this -> password = md5($data['password']);
+			 }
 //			 $this -> uname = $data['uname'];
-			 $data['password'] = md5($data['password']);
+//			 $data['password'] = md5($data['password']);
 //			 $this -> status = $data['status'];
 //	         $this -> user_id = $data['user_id'];
 //	         $this -> number = $data['number'];
